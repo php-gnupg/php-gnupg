b938cf17624424f4210b7de3207e82edf6d4cef2
cvs2svn <cvs2svn@php.net>
2005-11-12 16:53:41 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_0_6'.

--- examples/keylistiterator.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once    (dirname(__FILE__)."/main.php");
-
-$iterator	=	new gnupg_keylistiterator("php.net");
-foreach($iterator as $key => $value){
-	echo $key." -> ".$value."\n";
-}
-?>
