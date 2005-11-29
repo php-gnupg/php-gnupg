631a9890d963fc9f85bb8e390753f4091355fab5
Thilo Raufeisen <traufeisen@php.net>
2005-11-29 20:54:36 +0000

    - finally changed licence to BSD, revised - bumped date in package.xml

--- php_gnupg_keylistiterator.h
+++ php_gnupg_keylistiterator.h
@@ -1,15 +1,13 @@
 /*
-  +----------------------------------------------------------------------+
-  | Copyright (c) 2005 Thilo Raufeisen <traufeisen@php.net>              |
-  +----------------------------------------------------------------------+
-  | This source file is subject to version 3.01 of the PHP license,      |
-  | that is bundled with this package in the file LICENSE, and is        |
-  | available through the world-wide-web at the following url:           |
-  | http://www.php.net/license/3_01.txt.                                 |
-  | If you did not receive a copy of the PHP license and are unable to   |
-  | obtain it through the world-wide-web, please send a note to          |
-  | license@php.net so we can mail you a copy immediately.               |
-  +----------------------------------------------------------------------+
+  +--------------------------------------------------------------------+
+  | PECL :: gnupg                                                      |
+  +--------------------------------------------------------------------+
+  | Redistribution and use in source and binary forms, with or without |
+  | modification, are permitted provided that the conditions mentioned |
+  | in the accompanying LICENSE file are met.                          |
+  +--------------------------------------------------------------------+
+  | Copyright (c) 2005, Thilo Raufeisen <traufeisen@php.net>           |
+  +--------------------------------------------------------------------+
 */
 
 /* $Id$ */
